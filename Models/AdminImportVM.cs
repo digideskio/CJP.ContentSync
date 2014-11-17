b9dacecbb6b907ae94a820fcc29c262cf402731f
paynecrl97 <chris.payne@bedegaming.com>
2014-11-17 10:12:37 +0000

    added required attributes to vm

--- Models/AdminImportVM.cs
+++ Models/AdminImportVM.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
 
 namespace CJP.ContentSync.Models {
     public class AdminImportVM
@@ -7,8 +9,11 @@ namespace CJP.ContentSync.Models {
             SavedRemoteSiteConfigs = new List<RemoteSiteConfigRecord>();
         }
 
+        [Required]
         public string Url { get; set; }
+        [Required]
         public string Username { get; set; }
+        [Required]
         public string Password { get; set; }
         public List<RemoteSiteConfigRecord> SavedRemoteSiteConfigs { get; set; }
     }
