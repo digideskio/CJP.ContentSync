a52ebb4b6009a5ec656651b50598e0f17a937d5a
paynecrl97 <chris.payne@bedegaming.com>
2014-11-22 12:42:24 +0000

    added ability to import redactions as part of a recipe

---
+++ RecipeHandlers/RedactionsRecipeHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using CJP.ContentSync.Models;
+using CJP.ContentSync.Services;
+using Orchard.Localization;
+using Orchard.Logging;
+using Orchard.Recipes.Models;
+using Orchard.Recipes.Services;
+
+namespace CJP.ContentSync.RecipeHandlers {
+    public class RedactionsRecipeHandler : IRecipeHandler {
+        private readonly ITextRedactionService _textRedactionService;
+
+        public RedactionsRecipeHandler(ITextRedactionService textRedactionService)
+        {
+            _textRedactionService = textRedactionService;
+            Logger = NullLogger.Instance;
+            T = NullLocalizer.Instance;
+        }
+
+        public Localizer T { get; set; }
+        public ILogger Logger { get; set; }
+
+        /*  
+         <Redactions>
+          <add Regex="Production" Placeholder="EnvironmentName" ReplaceWith="Local" />
+         </Redactions>
+        */
+        // Add a set of redactions
+        public void ExecuteRecipeStep(RecipeContext recipeContext) {
+            if (!String.Equals(recipeContext.RecipeStep.Name, "Redactions", StringComparison.OrdinalIgnoreCase))
+            {
+                return;
+            }
+
+            var redactions = recipeContext.RecipeStep.Step.Descendants().Where(f => f.Name == "add");
+            foreach (var redaction in redactions)
+            {
+                _textRedactionService.AddRedaction(new RedactionRecord { Placeholder = redaction.Attribute("Placeholder").Value, Regex = redaction.Attribute("Regex").Value, ReplaceWith = redaction.Attribute("ReplaceWith").Value, });
+            }
+
+            recipeContext.Executed = true;
+        }
+    }
+}
