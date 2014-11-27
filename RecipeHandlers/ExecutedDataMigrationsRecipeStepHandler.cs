5a889ecce0a6281ab7b6ffa0e4772bf5835b9702
paynecrl97 <chris.payne@bedegaming.com>
2014-11-27 16:46:03 +0000

    added feedback to executed migrations and feature sync

--- RecipeHandlers/ExecutedDataMigrationsRecipeStepHandler.cs
+++ RecipeHandlers/ExecutedDataMigrationsRecipeStepHandler.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Linq;
+using CJP.ContentSync.ExtensionMethods;
 using CJP.ContentSync.Services;
+using Orchard.Localization;
 using Orchard.Recipes.Models;
 using Orchard.Recipes.Services;
 
@@ -9,10 +11,15 @@ namespace CJP.ContentSync.RecipeHandlers
     public class ExecutedDataMigrationsRecipeStepHandler : IRecipeHandler
     {
         private readonly IContentMigrationStateService _contentMigrationStateService;
+        private readonly IRealtimeFeedbackService _realtimeFeedbackService;
 
-        public ExecutedDataMigrationsRecipeStepHandler(IContentMigrationStateService contentMigrationStateService) {
+        public ExecutedDataMigrationsRecipeStepHandler(IContentMigrationStateService contentMigrationStateService, IRealtimeFeedbackService realtimeFeedbackService) {
             _contentMigrationStateService = contentMigrationStateService;
+            _realtimeFeedbackService = realtimeFeedbackService;
+
+            T = NullLocalizer.Instance;
         }
+        public Localizer T { get; set; }
 
         /*
           <ExecutedDataMigrations>
@@ -25,19 +32,38 @@ namespace CJP.ContentSync.RecipeHandlers
                 return;
             }
 
+            _realtimeFeedbackService.Info(T("Entering the 'Executed Data Migrations' step. This step will reset the local record of content migrations to the state of the remote server"));
+
             var migrations = recipeContext.RecipeStep.Step.Descendants();
-            var migrationNames = migrations.Where(f => f.Name == "Migration").Select(f => f.Attribute("Name").Value);
+            var migrationNames = migrations.Where(f => f.Name == "Migration").Select(f => f.Attribute("Name").Value).ToList();
+
+            _realtimeFeedbackService.Info(T("Found {0} migrations that have ran remotely:", migrationNames.Count()));
+            foreach (var migrationName in migrationNames)
+            {
+                _realtimeFeedbackService.Info(T(migrationName));
+            }
 
             var locallyRanMigrations = _contentMigrationStateService.GetExecutedMigrations().ToList();
 
-            foreach (var migration in locallyRanMigrations.Where(m=>!migrationNames.Contains(m))) {//migrations that have been executed locally, but not in the recipe that is been executed
+            _realtimeFeedbackService.Info(T("Found {0} migrations that have ran locally:", locallyRanMigrations.Count()));
+            foreach (var migrationName in locallyRanMigrations)
+            {
+                _realtimeFeedbackService.Info(T(migrationName));
+            }
+
+            foreach (var migration in locallyRanMigrations.Where(m => !migrationNames.Contains(m)))
+            {//migrations that have been executed locally, but not in the recipe that is being executed
+                _realtimeFeedbackService.Info(T("Marking migration '{0}' as pending", migration));
                 _contentMigrationStateService.MarkMigrationAsPending(migration);
             }
 
-            foreach (var migrationToAdd in migrationNames.Where(m=>!locallyRanMigrations.Contains(m))) {
+            foreach (var migrationToAdd in migrationNames.Where(m => !locallyRanMigrations.Contains(m)))
+            {
+                _realtimeFeedbackService.Info(T("Marking migration '{0}' as executed", migrationToAdd));
                 _contentMigrationStateService.MarkMigrationAsExecuted(migrationToAdd);
             }
 
+            _realtimeFeedbackService.Info(T("The 'Executed Data Migrations' step is finished"));
             recipeContext.Executed = true;
         }
     }
--- RecipeHandlers/FeatureSyncRecipeStepHandler.cs
+++ RecipeHandlers/FeatureSyncRecipeStepHandler.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Linq;
+using CJP.ContentSync.ExtensionMethods;
+using CJP.ContentSync.Services;
 using Orchard.Environment.Features;
+using Orchard.Localization;
 using Orchard.Recipes.Models;
 using Orchard.Recipes.Services;
 
@@ -9,11 +12,15 @@ namespace CJP.ContentSync.RecipeHandlers
     public class FeatureSyncRecipeStepHandler : IRecipeHandler
     {
         private readonly IFeatureManager _featureManager;
+        private readonly IRealtimeFeedbackService _realtimeFeedbackService;
 
-        public FeatureSyncRecipeStepHandler(IFeatureManager featureManager)
-        {
+        public FeatureSyncRecipeStepHandler(IFeatureManager featureManager, IRealtimeFeedbackService realtimeFeedbackService) {
             _featureManager = featureManager;
+            _realtimeFeedbackService = realtimeFeedbackService;
+
+            T = NullLocalizer.Instance;
         }
+        public Localizer T { get; set; }
 
         /*
           <EnabledFeatures>
@@ -26,23 +33,40 @@ namespace CJP.ContentSync.RecipeHandlers
                 return;
             }
 
+            _realtimeFeedbackService.Info(T("Entering the 'Feature Sync' step. This step ensures that the features enabled and disabled locally match those enabled and disabled remotely."));
+
             var features = recipeContext.RecipeStep.Step.Descendants();
-            var featureIds = features.Where(f => f.Name == "Feature").Select(f => f.Attribute("Id").Value);
+            var featureIds = features.Where(f => f.Name == "Feature").Select(f => f.Attribute("Id").Value).ToList();
+            _realtimeFeedbackService.Info(T("The remote site has {0} features enabled.", featureIds.Count()));
 
             var availableFeatures = _featureManager.GetAvailableFeatures();
             var enabledFeatures = _featureManager.GetEnabledFeatures().ToList();
 
-            var featuresToDisable = enabledFeatures.Where(f => !featureIds.Contains(f.Id))
-                .Select(f => f.Id);
+            var featuresToDisable = enabledFeatures.Where(f => !featureIds.Contains(f.Id)).Select(f => f.Id).ToList();
+            _realtimeFeedbackService.Info(T("{0} feature(s) are due to be disabled:", featuresToDisable.Count()));
+            foreach (var featureToDisable in featuresToDisable)
+            {
+                _realtimeFeedbackService.Info(T(featureToDisable));
+            }
 
             var featuresToEnable = availableFeatures
                 .Where(f => featureIds.Contains(f.Id)) //available features that are in the list of features that need to be enabled
                 .Where(f => !enabledFeatures.Select(ef => ef.Id).Contains(f.Id)) //remove features that are already enabled
-                .Select(f => f.Id);
+                .Select(f => f.Id)
+                .ToList();
+            _realtimeFeedbackService.Info(T("{0} feature(s) are due to be enabled.", featuresToEnable.Count()));
+            foreach (var featureToEnable in featuresToEnable)
+            {
+                _realtimeFeedbackService.Info(T(featureToEnable));
+            }
 
+            _realtimeFeedbackService.Info(T("Disabling the following features: ", string.Join(", ", featuresToDisable)));
             _featureManager.DisableFeatures(featuresToDisable, true);
+
+            _realtimeFeedbackService.Info(T("Enabling the following features: ", string.Join(", ", featuresToEnable)));
             _featureManager.EnableFeatures(featuresToEnable, true);
 
+            _realtimeFeedbackService.Info(T("Feature Sync has completed"));
             recipeContext.Executed = true;
         }
     }
